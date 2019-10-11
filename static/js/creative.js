292009b934fa90727dac6e1cac72c8bc26a8ff79
GE20070469@wipro.com <gelmis.bartulis@wipro.com>
2019-10-11 15:55:57 +0100

    publish 2 is ready

--- static/js/creative.js
+++ static/js/creative.js
@@ -4,6 +4,7 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+
 (function($) {
     "use strict"; // Start of use strict
 
@@ -74,21 +75,21 @@ $( document ).ready(function() {
 	var avatar = $('.my-avatar');
 	var header = $('header');
 	
-	avatar.mouseover( function() {
-		$('.overlay').fadeIn(1000);
-		$('.social-links').css('background', 'transparent');
-		$('.header-content-inner h1').css('background', 'transparent').fadeIn();
-		$('.header-content-inner h3').css('background', 'transparent').fadeIn();
-	});
-	
-	
-	avatar.mouseout( function() {
-		$('.overlay').fadeOut(1000);
-		$('.social-links').css('background', 'linear-gradient(90deg, rgba(34,34,34,0) 0%, rgba(34,34,34,0.75) 30%, rgba(24,24,24,1) 50%, rgba(34,34,34,0.75) 70%, rgba(34,34,34,0) 100%)');
-		$('.header-content-inner h1').css('background', 'linear-gradient(90deg, rgba(34,34,34,0) 0%, rgba(34,34,34,0.75) 30%, rgba(24,24,24,1) 50%, rgba(34,34,34,0.75) 70%, rgba(34,34,34,0) 100%)').fadeIn();
-		$('.header-content-inner h3').css('background', 'linear-gradient(90deg, rgba(34,34,34,0) 0%, rgba(34,34,34,0.75) 30%, rgba(24,24,24,1) 50%, rgba(34,34,34,0.75) 70%, rgba(34,34,34,0) 100%)').fadeIn();
-	});
-	
+//	avatar.mouseover( function() {
+//		$('.overlay').fadeIn(1000);
+//		$('.social-links').css('background', 'transparent');
+//		$('.header-content-inner h1').css('background', 'transparent').fadeIn();
+//		$('.header-content-inner h3').css('background', 'transparent').fadeIn();
+//	});
+//	
+//	
+//	avatar.mouseout( function() {
+//		$('.overlay').fadeOut(1000);
+//		$('.social-links').css('background', 'linear-gradient(90deg, rgba(34,34,34,0) 0%, rgba(34,34,34,0.75) 30%, rgba(24,24,24,1) 50%, rgba(34,34,34,0.75) 70%, rgba(34,34,34,0) 100%)');
+//		$('.header-content-inner h1').css('background', 'linear-gradient(90deg, rgba(34,34,34,0) 0%, rgba(34,34,34,0.75) 30%, rgba(24,24,24,1) 50%, rgba(34,34,34,0.75) 70%, rgba(34,34,34,0) 100%)').fadeIn();
+//		$('.header-content-inner h3').css('background', 'linear-gradient(90deg, rgba(34,34,34,0) 0%, rgba(34,34,34,0.75) 30%, rgba(24,24,24,1) 50%, rgba(34,34,34,0.75) 70%, rgba(34,34,34,0) 100%)').fadeIn();
+//	});
+//	
 	
 	
 	var str = 'As you already guessed, my name is Gelmis. I was born in this beautiful country called Lithuania 23 years ago. Since one of my family members have moved to Ireland I used to come to visit them during my school holidays. One summer I stayed started going to school in Dublin. Since (at the time) my hobbies had two things in common (creating and problem solving) - as soon as I finished school I delved into college to study Computer Science. I graduated without failing a single subject for 4 years and I was given an oportunity to present my BSc. (Hons) research at International Conference on meaning and knowledge representation, which I had prepared for a year (okay, this is as far as my ego goes, I swear). Whilst studying, I began looking for a job within my field. And finally found one! First, official job in my field. An intership position as a Full - Stack Android developer for MABS. Where I was crafting a prototype financing application for 3 months. As the intership was coming to an end, I was quite bored, since I was unable to get another job straight away. Between a very rare chance of freelancing and the odd exploration trip it felt quite dull. So, I picked up photography, and mostly got inspired by my godfather.While I\'m not expert, I\'m trying  and as Mark Manson once said - "Don’t just sit there. Do something. The answers will follow". So I guess photography is kind of my side hobby for when I travel or feel creative. You can see some of my attempts in the links below! (I\'m in progress of developing a photo portfolio / blog kind of a website). That sums up my story. What about you?';
@@ -105,8 +106,18 @@ $( document ).ready(function() {
 	});
 	
 	
+	
+	$(".headerH1").addClass("tracking-in-contract-bck").delay(2000).queue(function(){
+		$(this).removeClass("tracking-in-contract-bck").dequeue();
+		$(this).addClass("blur-out-contract").hide(2000);
+		$('.header-content-inner').addClass("tracking-in-contract-bck").fadeIn(2000);
+	});
 });
+
+
 $(document).ready(function() { 
+	
+	
 
 	(function ($) { 
 		$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
@@ -124,12 +135,13 @@ $(document).ready(function() {
 			g.preventDefault();
 		} );
 	})(jQuery);
-
+	
 });
 
 $(window).load(function () {
     // Animate loader off screen
     $(".gooeyHolder").fadeOut("fast");
 });
+
 
 
